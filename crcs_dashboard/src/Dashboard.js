090e61077c661e37139bbd3f12f60dc05f24a842
Rhyma <rhymataylor@gmail.com>
2023-06-17 00:34:17 +0530

    Minor fixes

--- crcs_dashboard/src/Dashboard.js
+++ crcs_dashboard/src/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
         <>
             <Navbar />
             <div className="card-container">
-                <Card value="100" label="Registered Socities" route="/SocietiesTable"/>
+                <Card value="100" label="Registered Societies" route="/SocietiesTable"/>
                 <Card value="18" label="States in India" route="/StatesPieChart"/>
                 <Card value="13" label="Sectors" route="/SectorsPieChart"/>
             </div>
@@ -19,8 +19,8 @@ const Dashboard = () => {
                         <li><button><a href="https://cooperation.gov.in/user/login">Login</a></button></li>
                         <li><button><a href="https://mscs.dac.gov.in/Form1.aspx" >Register New Society</a></button></li>
                         <li><button><a href="https://mscs.dac.gov.in/LiquidationList.aspx">Liquidation List</a></button></li>
-                        <li><button><a href="/StackedBarChart">State wise Sector Distribution</a></button></li>
-                        <li><button><a href="/YearWiseChart" >Year Wise Registration</a></button></li>
+                        <li><button><a href="/StackedBarChart">State Wise Sector Distribution</a></button></li>
+                        <li><button><a href="/YearWiseChart" >Year Wise Registrations</a></button></li>
                     </ul>
                 </div>
             </div>
